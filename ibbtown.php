b1e46d27422704293004ab3a06a09fb0af2cc8ad
PetersOtto <52236754+PetersOtto@users.noreply.github.com>
2026-02-09 16:44:40 +0100

    Update ibbtown.php

--- ibbtown.php
+++ ibbtown.php
@@ -19,14 +19,4 @@ class YellowIbbtown {
             $this->yellow->system->save($fileName, array("theme" => $this->yellow->system->getDifferent("theme")));
         }
     }
-
-    // Handle enumeration
-    public function onEnumerate($action, $context) {
-        $output = null;
-        if ($action=="theme") {
-            $path = $this->yellow->system->get("coreThemeDirectory");
-            $output = $this->yellow->toolbox->getDirectoryEntries($path, "/.*/", false, true, false);
-        }
-        return $output;
-    }
 }
